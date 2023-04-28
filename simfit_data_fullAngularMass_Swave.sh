deb8c9e62fa3ce7c9e78486b1f9e580a5d72852e
Alessio Boletti <alessio.boletti@cern.ch>
2023-04-28 18:01:21 +0200

    Use remote files as default for interactive data fits

--- simfit_data_fullAngularMass_Swave.sh
+++ simfit_data_fullAngularMass_Swave.sh
@@ -6,6 +6,8 @@ multi=0
 nsam=0
 q2stat=0
 
+localFile=0
+
 plot=1
 save=2
 
@@ -29,27 +31,27 @@ if make simfit_data_fullAngularMass_Swave; then
     case "$yearConf" in
 
 	0)
-	    echo ./simfit_data_fullAngularMass_Swave ${bin} ${par} ${multi} ${nsam} ${q2stat} ${XGBv} ${fitopt} 1 ${plot} ${save} 2016 2017 2018
-	    ./simfit_data_fullAngularMass_Swave ${bin} ${par} ${multi} ${nsam} ${q2stat} ${XGBv} ${fitopt} 1 ${plot} ${save} 2016 2017 2018 \
-		&> logs_simFit4d/simfit_data_fullAngularMass_Swave_${bin}_${par}_${multi}_${nsam}_${q2stat}_${XGBv}_${fitopt}_1_${plot}_${save}_2016_2017_2018.log &
+	    echo ./simfit_data_fullAngularMass_Swave ${bin} ${par} ${multi} ${nsam} ${q2stat} ${XGBv} ${fitopt} ${localFile} ${plot} ${save} 2016 2017 2018
+	    ./simfit_data_fullAngularMass_Swave ${bin} ${par} ${multi} ${nsam} ${q2stat} ${XGBv} ${fitopt} ${localFile} ${plot} ${save} 2016 2017 2018 \
+		&> logs_simFit4d/simfit_data_fullAngularMass_Swave_${bin}_${par}_${multi}_${nsam}_${q2stat}_${XGBv}_${fitopt}_${localFile}_${plot}_${save}_2016_2017_2018.log &
 	    ;;
 
 	1)
-	    echo ./simfit_data_fullAngularMass_Swave ${bin} ${par} ${multi} ${nsam} ${q2stat} ${XGBv} ${fitopt} 1 ${plot} ${save} 2016
-	    ./simfit_data_fullAngularMass_Swave ${bin} ${par} ${multi} ${nsam} ${q2stat} ${XGBv} ${fitopt} 1 ${plot} ${save} 2016 \
-		&> logs_simFit4d/simfit_data_fullAngularMass_Swave_${bin}_${par}_${multi}_${nsam}_${q2stat}_${XGBv}_${fitopt}_1_${plot}_${save}_2016.log &
+	    echo ./simfit_data_fullAngularMass_Swave ${bin} ${par} ${multi} ${nsam} ${q2stat} ${XGBv} ${fitopt} ${localFile} ${plot} ${save} 2016
+	    ./simfit_data_fullAngularMass_Swave ${bin} ${par} ${multi} ${nsam} ${q2stat} ${XGBv} ${fitopt} ${localFile} ${plot} ${save} 2016 \
+		&> logs_simFit4d/simfit_data_fullAngularMass_Swave_${bin}_${par}_${multi}_${nsam}_${q2stat}_${XGBv}_${fitopt}_${localFile}_${plot}_${save}_2016.log &
 	    ;;
 
 	2)
-	    echo ./simfit_data_fullAngularMass_Swave ${bin} ${par} ${multi} ${nsam} ${q2stat} ${XGBv} ${fitopt} 1 ${plot} ${save} 2017
-	    ./simfit_data_fullAngularMass_Swave ${bin} ${par} ${multi} ${nsam} ${q2stat} ${XGBv} ${fitopt} 1 ${plot} ${save} 2017 \
-		&> logs_simFit4d/simfit_data_fullAngularMass_Swave_${bin}_${par}_${multi}_${nsam}_${q2stat}_${XGBv}_${fitopt}_1_${plot}_${save}_2017.log &
+	    echo ./simfit_data_fullAngularMass_Swave ${bin} ${par} ${multi} ${nsam} ${q2stat} ${XGBv} ${fitopt} ${localFile} ${plot} ${save} 2017
+	    ./simfit_data_fullAngularMass_Swave ${bin} ${par} ${multi} ${nsam} ${q2stat} ${XGBv} ${fitopt} ${localFile} ${plot} ${save} 2017 \
+		&> logs_simFit4d/simfit_data_fullAngularMass_Swave_${bin}_${par}_${multi}_${nsam}_${q2stat}_${XGBv}_${fitopt}_${localFile}_${plot}_${save}_2017.log &
 	    ;;
 
 	3)
-	    echo ./simfit_data_fullAngularMass_Swave ${bin} ${par} ${multi} ${nsam} ${q2stat} ${XGBv} ${fitopt} 1 ${plot} ${save} 2018
-	    ./simfit_data_fullAngularMass_Swave ${bin} ${par} ${multi} ${nsam} ${q2stat} ${XGBv} ${fitopt} 1 ${plot} ${save} 2018 \
-		&> logs_simFit4d/simfit_data_fullAngularMass_Swave_${bin}_${par}_${multi}_${nsam}_${q2stat}_${XGBv}_${fitopt}_1_${plot}_${save}_2018.log &
+	    echo ./simfit_data_fullAngularMass_Swave ${bin} ${par} ${multi} ${nsam} ${q2stat} ${XGBv} ${fitopt} ${localFile} ${plot} ${save} 2018
+	    ./simfit_data_fullAngularMass_Swave ${bin} ${par} ${multi} ${nsam} ${q2stat} ${XGBv} ${fitopt} ${localFile} ${plot} ${save} 2018 \
+		&> logs_simFit4d/simfit_data_fullAngularMass_Swave_${bin}_${par}_${multi}_${nsam}_${q2stat}_${XGBv}_${fitopt}_${localFile}_${plot}_${save}_2018.log &
 	    ;;
 
     esac
